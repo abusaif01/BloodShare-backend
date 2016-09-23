3959655b7e2bbb2a9ff5f8344e0a7b034f6a0842
SAIF <abu.saif01@gmail.com>
2016-09-24 01:41:13 +0600

    Helper Build

--- src/com/bloodshare/controller/HelpController.java
+++ src/com/bloodshare/controller/HelpController.java
@@ -1,7 +1,11 @@
 package com.bloodshare.controller;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +18,41 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @RestController
 public class HelpController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HelpController.class);
 	private final RequestMappingHandlerMapping handlerMapping;
 	 
 	 @Autowired
 	 public HelpController(RequestMappingHandlerMapping handlerMapping) {
 	  this.handlerMapping = handlerMapping;
 	 }
-	  
-	 @RequestMapping(value="/endpointdoc", method=RequestMethod.GET)
+	 
+	 @RequestMapping(value="/help", method=RequestMethod.GET)
 	 public String show(Model model) {
+		 StringBuilder sb=new StringBuilder();
 		 
 		 Map<RequestMappingInfo, HandlerMethod> handlers = this.handlerMapping.getHandlerMethods();
+		 Set<RequestMappingInfo> keys = handlers.keySet();
+		 Iterator<RequestMappingInfo> it = keys.iterator();
+		 while (it.hasNext()) {
+			RequestMappingInfo requestMappingInfo = (RequestMappingInfo) it.next();
+			sb.append(requestMappingInfo.getPatternsCondition());
+			sb.append("\t\t----");
+			sb.append(requestMappingInfo.getMethodsCondition());
+			sb.append("\t\t----");
+			sb.append(requestMappingInfo.getHeadersCondition() );
+			sb.append("\t\t----");
+			sb.append(requestMappingInfo.getConsumesCondition());
+			sb.append("\t\t----");
+			sb.append(requestMappingInfo.getProducesCondition());
+			sb.append("<br/>");
+		}
 		 
-		 
-		 
-		 
-		 return  this.handlerMapping.getHandlerMethods().toString();
+		return  sb.toString();
 	 }
 	 
+	 @RequestMapping("/hello")
+    public String greeting() {
+        logger.debug("Every Thing is working . Ready to GO.");
+		return "Hello From Blood Share ";
+    }
 }

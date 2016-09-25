ef1d40559c05e9e488a92714bcc5cc78e79eb2f6
SAIF <abu.saif01@gmail.com>
2016-09-25 23:15:53 +0600

    test changes

--- src/test/java/test/bloodshare/database/Main.java
+++ src/test/java/test/bloodshare/database/Main.java
@@ -1,31 +1,17 @@
 package test.bloodshare.database;
 
-import java.util.Date;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import com.bloodshare.entity.Donor;
+import com.bloodshare.dao.DonorDAO;
+import com.bloodshare.dao.DonorDAOImpl;
 import com.bloodshare.util.HibernateUtil;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-//		Session session = HibernateUtil.getSession();
-//		Transaction transaction = session.beginTransaction();
-////		User user=new User();
-////		
-////		user.setName("test");
-////		user.setBirthDate(new Date());
-////		user.setId("xtz");
-////		user.setMobile("2136554");
-////		user.setBloodGroup("0+");
-////		session.save(user);
-//		Donor user=(Donor) session.get(Donor.class , "abc");
-//		System.out.println(user);
-//		transaction.commit();
-//		HibernateUtil.closeSession(session);
+		DonorDAO donorDAO=new DonorDAOImpl();
+		donorDAO.setSessionFactory(HibernateUtil.getSessionFactory());
+		System.out.println("\n\n\n");
+		System.out.println(donorDAO.read("abc"));
+		System.out.println("\n\n\n");
 	}
 	
 }

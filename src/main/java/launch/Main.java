9448fe44b228396ed340acfa967b89234c31a23b
Saif <abu.saif01@gmail.com>
2016-09-29 15:17:50 +0600

    kill process added :D

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,6 +1,8 @@
 package launch;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.core.StandardContext;
@@ -10,8 +12,49 @@ import org.apache.catalina.webresources.StandardRoot;
 
 public class Main {
 
+	private static void killProcess()
+	{
+		try {
+    	    String line;
+    	    String pid = null;
+    	    Process p = Runtime.getRuntime().exec
+    	    	    (System.getenv("windir") +"\\system32\\"+"netstat -nao");
+    	    BufferedReader input =
+    	            new BufferedReader(new InputStreamReader(p.getInputStream()));
+    	    while ((line = input.readLine()) != null) {
+    	    	if(line.contains("8080"))
+    	    	{
+    	    		System.out.println(line);
+    	    		String[] splited = line.split(" ");
+    	    		for (int i = 0; i < splited.length; i++) {
+						if(!splited[i].isEmpty())
+						{
+//							System.out.println("<------  " + splited[i] + " ------>");
+							if(splited[i].matches("[0-9]{4}"))
+							{
+								System.out.println("<------ pidFOund :" + splited[i] + " ------>");
+								pid=String.valueOf(Integer.parseInt(splited[i]));
+							}
+						}
+					}
+    	    	}
+    	        
+    	    }
+    	    input.close();
+    	    if(pid!=null)
+    	    {
+	    	    String cmd = "taskkill /F /PID " + pid;
+	    	    Runtime.getRuntime().exec(System.getenv("windir") +"\\system32\\"+cmd);
+    	    }
+    	    
+    	} catch (Exception err) {
+    	    err.printStackTrace();
+    	}
+	}
+	
     public static void main(String[] args) throws Exception {
 
+    	killProcess();
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
 

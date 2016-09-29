38b35ac5063ebd47b7c82c48058228779e7f6456
Saif <abu.saif01@gmail.com>
2016-09-29 12:57:20 +0600

    authentication fix

--- src/main/java/com/bloodshare/dao/DonorOtpDAOImpl.java
+++ src/main/java/com/bloodshare/dao/DonorOtpDAOImpl.java
@@ -3,19 +3,22 @@ package com.bloodshare.dao;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.NonUniqueObjectException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.bloodshare.entity.Donor;
 import com.bloodshare.entity.DonorOtp;
 
 @Repository
 public class DonorOtpDAOImpl implements DonotOtpDAO
 {
 
+	private static final Logger logger = LoggerFactory.getLogger(DonorOtpDAOImpl.class);
 	private SessionFactory sessionFactory;
 	
 	@Autowired
@@ -29,31 +32,39 @@ public class DonorOtpDAOImpl implements DonotOtpDAO
 	}
 
 	@Override
-	public DonorOtp update(DonorOtp t) {
+	public DonorOtp update(DonorOtp donorOtp) {
 		throw new UnsupportedOperationException("Update not supported for OTP");
 	}
 
 	@Override
-	public boolean save(DonorOtp t) {
+	public boolean save(DonorOtp donorOtp) {
 		Session session = sessionFactory.getCurrentSession();
-		session.saveOrUpdate(t);
+		session.saveOrUpdate(donorOtp);
 		return true;
 	}
 
 	@Override
-	public boolean delete(DonorOtp t) {
+	public boolean delete(DonorOtp donorOtp) {
 		Session session = sessionFactory.getCurrentSession();
-		session.delete(t);
+		try{
+			session.delete(donorOtp);
+		}catch(NonUniqueObjectException e)
+		{
+			logger.debug(e.getMessage());
+			logger.debug("Merging with seesion");
+			donorOtp=(DonorOtp) session.merge(donorOtp);
+			session.delete(donorOtp);
+		}
 		return true;
 	}
 
 	@Override
-	public boolean isExist(DonorOtp otp) {
+	public boolean isExist(DonorOtp donorOtp) {
 		
 		Session session=sessionFactory.getCurrentSession();
 		Criteria cr = session.createCriteria(DonorOtp.class);
-		cr.add(Restrictions.eq("mobile", otp.getMobile()));
-		cr.add(Restrictions.eq("key", otp.getKey()));
+		cr.add(Restrictions.eq("mobile", donorOtp.getMobile()));
+		cr.add(Restrictions.eq("key", donorOtp.getKey()));
 		List list = cr.list();
 		
 		return (list!=null && list.size()==1);
--- src/main/java/com/bloodshare/entity/DonorOtp.java
+++ src/main/java/com/bloodshare/entity/DonorOtp.java
@@ -28,5 +28,37 @@ public class DonorOtp {
 	public void setKey(String key) {
 		this.key = key;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DonorOtp other = (DonorOtp) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (mobile == null) {
+			if (other.mobile != null)
+				return false;
+		} else if (!mobile.equals(other.mobile))
+			return false;
+		return true;
+	}
+	
 	
 }

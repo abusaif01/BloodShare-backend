ecfba1d76871f0f972e2bd79b30053dcf626c9c6
SAIF <abu.saif01@gmail.com>
2017-09-13 23:51:39 +0600

    configuration done all working good

--- src/main/java/com/bloodshare/SpringApplicationLauncher.java
+++ src/main/java/com/bloodshare/SpringApplicationLauncher.java
@@ -1,12 +1,25 @@
 package com.bloodshare;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
+@ComponentScan("com")
 public class SpringApplicationLauncher {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApplicationLauncher.class, args);
+	}
+	
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+	    HibernateJpaSessionFactoryBean fact = new HibernateJpaSessionFactoryBean();
+	    fact.setEntityManagerFactory(emf);
+	    return fact;
 	}
 }

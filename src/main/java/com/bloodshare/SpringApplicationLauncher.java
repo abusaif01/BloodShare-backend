51f255ca9680e5bd0122c580b9bf25dac8cb03fa
Saif <abu.saif01@gmail.com>
2017-09-13 14:32:50 +0600

    Adding spring boot try 1

---
+++ src/main/java/com/bloodshare/SpringApplicationLauncher.java
@@ -0,0 +1,12 @@
+package com.bloodshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationLauncher.class, args);
+	}
+}

0fea00897d27eb7768b7a2657c62a54e0443039e
Saif <abu.saif01@gmail.com>
2017-09-19 18:08:08 +0600

    location 1

--- src/main/java/com/adrjun/search/LocationBasedSearch.java
+++ src/main/java/com/adrjun/search/LocationBasedSearch.java
@@ -1,6 +1,10 @@
 package com.adrjun.search;
 
 public class LocationBasedSearch {
-	private static final int SEARCH_PARAMETER=10;
+	private static final int SEARCH_PARAMETER_IN_KM=2;
 
+	private float covertKMintoGeoDistance(int km)
+	{
+		
+	}
 }
